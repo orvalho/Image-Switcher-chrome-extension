3cd7e72738293c461c9f37a8e2bcc5578a89c744
orvalho <rasa396@gmail.com>
2018-03-22 23:53:49 +0200

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,3 @@
+for (var i = 0; document.images.length; i++) {
+  document.images[i].src = "https://cdn.pixabay.com/photo/2018/03/19/20/20/frog-3241108_960_720.png"; // this is url address for frog image.
+};
